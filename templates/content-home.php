49f8c5e9786f0980955b2ef91bf06b9817265179
Ellie Day <heyellieday@gmail.com>
2014-08-10 15:17:39 -0400

    Initial Commit

---
+++ templates/content-home.php
@@ -0,0 +1,12 @@
+<div class="home-header">
+</div>
+<div class="how-it-works">
+</div>
+<div class="call-to-action">
+</div>
+<div class="stats">
+</div>
+<div class="mission-statement">
+</div>
+<div class="message-from-ceo">
+</div>
