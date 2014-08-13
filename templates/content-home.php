2c22ce29f3f1fa7412f03154a8be51c7971f10a9
Ellie Day <heyellieday@gmail.com>
2014-08-13 15:31:00 -0500

    Did more work on home page

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,12 +1,96 @@
 <div class="home-header">
+	<div class="row">
+		<div class="col-sm-2">
+		</div>
+		<div class="col-sm-4">
+			<img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-tippity.png">
+			<h1 class="slogan">Be Fashion and Weather Savvy<span class="hashtag"> #fashionforecast</span></h1>
+		</div>
+		<div class="col-sm-1">
+		</div>
+		<div class="col-sm-3 hidden-xs">
+			<img class="iphone pull-right" src="<?php echo get_template_directory_uri(); ?>/assets/img/iphone.png" >
+		</div>
+		<div class="col-sm-2">
+		</div>
+	</div>
 </div>
 <div class="how-it-works">
+	<div class="container">
+		<h1 class="section-header">How It Works</h1>
+	</div>
+	<div class="row">
+		<div class="steps">
+			<div class="col-sm-4 step">
+				<h1>1</h1>
+				<h4>Enter your local zip code</h4>
+				<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat, arcu ut interdum posuere, nibh dui sollicitudin nibh, et elementum ligula erat in diam. Aliquam vestibulum risus interdum, commodo justo sit amet, accumsan augue. Vestibulum sit amet facilisis mauris, nec elementum justo.</h5>
+			</div>
+			<div class="col-sm-4 step">
+				<h1>2</h1>
+				<h4>Weather+Fashion</h4>
+				<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat, arcu ut interdum posuere, nibh dui sollicitudin nibh, et elementum ligula erat in diam. Aliquam vestibulum risus interdum, commodo justo sit amet, accumsan augue. Vestibulum sit amet facilisis mauris, nec elementum justo.</h5>
+			</div>
+			<div class="col-sm-4 step">
+				<h1>3</h1>
+				<h4>Get Dressed</h4>
+				<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat, arcu ut interdum posuere, nibh dui sollicitudin nibh, et elementum ligula erat in diam. Aliquam vestibulum risus interdum, commodo justo sit amet, accumsan augue. Vestibulum sit amet facilisis mauris, nec elementum justo.</h5>
+			</div>
+		</div>
+	</div>
 </div>
 <div class="call-to-action">
+	<div class="container">
+		<h2 class="cta-header">
+		Are you ready to be fashion and weather savvy?</h2>
+		<a href="#" class="subscribe"><h2>
+		I'm ready!</h2></a>
+	</div>
 </div>
 <div class="stats">
+	<div class="col-sm-2">
+		</div>
+	<div class="col-sm-2 stat">
+		<h1>186+</h1>
+		<h4>Outfit changes due to weather</h4>
+	</div>
+	<div class="col-sm-2 stat">
+		<h1>321+</h1>
+		<h4>Shoes ruined daily</h4>
+	</div>
+	<div class="col-sm-2 stat">
+		<h1>43+</h1>
+		<h4>Hours looking for the perfect dress</h4>
+	</div>
+	<div class="col-sm-2 stat">
+		<h1>1000+</h1>
+		<h4>Great fashion recomendations not being shared daily</h4>
+	</div>
+	<div class="col-sm-2">
+	</div>
 </div>
 <div class="mission-statement">
+	<div class="container">
+		<h1 class="section-header">Mission Statement</h1>
+		<div class="col-sm-2">
+		</div>
+		<div class="col-sm-8">
+			<h5 class="section-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat, arcu ut interdum posuere, nibh dui sollicitudin nibh, et elementum ligula erat in diam. Aliquam vestibulum risus interdum, commodo justo sit amet, accumsan augue. Vestibulum sit amet facilisis mauris, nec elementum justo. Nulla dignissim lorem quam, sed facilisis tortor commodo vel. Cras mattis venenatis aliquet. Phasellus eget dignissim velit, sit amet porttitor metus. Quisque blandit ligula vel nibh convallis, vitae vulputate enim sodales. Etiam euismod vulputate lacus, fermentum tincidunt diam tempus sit amet. Maecenas a imperdiet nunc. Sed facilisis arcu mi, nec malesuada est aliquam et.</h5>
+		</div>
+		<div class="col-sm-2">
+		</div>
+	</div>
 </div>
 <div class="message-from-ceo">
+	<div class="col-sm-2">
+		</div>
+		<div class="col-sm-3">
+			<img class="img-ceo img-circle img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/ceo.jpg">
+		</div>
+		<div class="col-sm-5 message">
+			<h1>Message from our CEO</h1>
+			<h5>Vestibulum sit amet facilisis mauris, nec elementum justo. Nulla dignissim lorem quam, sed facilisis tortor commodo vel. Cras mattis venenatis aliquet. Phasellus eget dignissim velit, sit amet porttitor metus. Quisque blandit ligula vel nibh convallis, vitae vulputate enim sodales. Etiam euismod vulputate lacus, fermentum tincidunt diam tempus sit amet. Maecenas a imperdiet nunc. Sed facilisis arcu mi, nec malesuada est aliquam et.</h5>
+		</div>
+		<div class="col-sm-2">
+		</div>
 </div>
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,6 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
+  	<img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-tippity.png">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
